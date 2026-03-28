802371fc619fe74a118cff3089d80d8b84f81cfb
langkeyo <2608563915@qq.com>
2026-03-28 18:04:11 +0800

    build: setup project tools and configs

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import tailwindcss from '@tailwindcss/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({ resolvers: [ElementPlusResolver()], dts: true }),
+    Components({ resolvers: [ElementPlusResolver()], dts: true }),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
